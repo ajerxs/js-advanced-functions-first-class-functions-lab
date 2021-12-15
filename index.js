0b72a07f3d3a790c98c81d35f552f10f34f55125
Andrew Eames <ajerxs@gmail.com>
2021-12-14 19:10:49 -0500

    commit tree 055ec323c09150132629d0454f67e9120984d84b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return integer * fare;
+    };
+};
+
+function fareDoubler(fare) {
+    const doubled = createFareMultiplier(2)(fare);
+    return doubled;
+};
+
+function fareTripler(fare) {
+    const tripled = createFareMultiplier(3)(fare);
+    return tripled;
+};
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+};
